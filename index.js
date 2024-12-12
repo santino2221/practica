aab2492c842f6ddd1dab87aa2e767c5c106a6a04
santino2221 <166014288+santino2221@users.noreply.github.com>
2024-12-12 09:09:46 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let n1 = Number (prompt('Ingresa un valor') )
+let n2 =  Number (prompt('Ingresa un valor') ) 
+let nombre = prompt ("ingresa un nombre")
+console.log (n1+n2) 
+console.log (n1-n2) 
+console.log (n1*n2)
+console.log  (n1/n2)
+alert("hola" + nombre)
+
+if (condition) {
+    alert("estas aprobado")
+    
+} else {
+  alert("estas desaprobado")  
+}
